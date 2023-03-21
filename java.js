e6a295e26d795820a5301989f17b698281b5f4d3
Roberto <robertorebolledo151@gmail.com>
2023-03-21 12:37:49 +0100

    javascript

---
+++ java.js
@@ -0,0 +1,51 @@
+alert('Soy un chulo')
+var edad = 5
+let mes = "Enero" /*Este es el wapo molon*/ 
+
+alert(edad +' de '+mes)
+alert('Joe tela de '+pan)
+
+function miFuncion(){
+    if(true){
+        var edad= 18
+        let telefono = 1234567
+    }
+    /*alert(var); no va */
+    /*alert(telefono); va*/
+}
+
+/*TIPOS DE DATOS */
+
+/*Numeros */
+
+let iva = 21, precio = 23.95
+
+/*string */
+
+let pan = 'Panes'
+
+/*Booleanos*/
+
+let verda= true;
+
+
+/*ARRAYS */
+
+let panes = new Array();
+let panesVerda = [pan,verda,iva,precio];
+
+alert(panesVerda.toString)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
