6f4eb7dea4cf529dec87d6c391d51b6e9f15ccaf
Roberto <robertorebolledo151@gmail.com>
2023-03-22 10:00:43 +0100

    ejercicios javascript

--- java.js
+++ java.js
@@ -1,9 +1,9 @@
-alert('Soy un chulo')
+/*alert('Soy un chulo')
 var edad = 5
 let mes = "Enero" /*Este es el wapo molon*/ 
-
+/*
 alert(edad +' de '+mes)
-alert('Joe tela de '+pan)
+alert('Joe tela de pan')
 
 function miFuncion(){
     if(true){
@@ -11,25 +11,25 @@ function miFuncion(){
         let telefono = 1234567
     }
     /*alert(var); no va */
-    /*alert(telefono); va*/
+    /*alert(telefono); va*//*
 }
 
 /*TIPOS DE DATOS */
 
-/*Numeros */
+/*Numeros *//*
 
 let iva = 21, precio = 23.95
 
-/*string */
+/*string *//*
 
 let pan = 'Panes'
 
-/*Booleanos*/
+/*Booleanos*//*
 
 let verda= true;
 
 
-/*ARRAYS */
+/*ARRAYS *//*
 
 let panes = new Array();
 let panesVerda = [pan,verda,iva,precio];
@@ -37,7 +37,24 @@ let panesVerda = [pan,verda,iva,precio];
 alert(panesVerda.toString)
 
 
+let pass1 = 1234
+let pass2 = 1235
 
+let mensaje= (pass1==pass2)?"Contraseña correca":"Contraseña incorrecta"
+alert(mensaje)
+*/
+let mensaje1 =["Pan","panes","joe"]
+
+for(let letra of mensaje1){
+    alert(letra)
+}
+let punto = prompt("punto"),ge =prompt("ge")
+
+alert(quevedo(punto,ge))
+
+function quevedo (punto, ge){
+    return( punto+" "+ge)
+}
 
 
 
