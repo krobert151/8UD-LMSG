0b29ef7dcf47a7d75fe2fd8400eb74892c0eea23
Roberto Rebolledo <robertorebolledo151@gmail.com>
2023-04-13 20:56:08 +0200

    colorinchios

--- java.js
+++ java.js
@@ -1,6 +1,6 @@
 /*alert('Soy un chulo')
 var edad = 5
-let mes = "Enero" /*Este es el wapo molon*/ 
+let mes = "Enero" /*Este es el wapo molon*/
 /*
 alert(edad +' de '+mes)
 alert('Joe tela de pan')
@@ -11,7 +11,7 @@ function miFuncion(){
         let telefono = 1234567
     }
     /*alert(var); no va */
-    /*alert(telefono); va*//*
+/*alert(telefono); va*//*
 }
 
 /*TIPOS DE DATOS */
@@ -51,20 +51,7 @@ let pass2 = 1235
 let mensaje= (pass1==pass2)?"Contraseña correca":"Contraseña incorrecta"
 alert(mensaje)
 */
-let palabras = new Array();
-let salir = false;
-do{
-    let palabra = prompt("Diga una palabra");
-    if(palabra!="0"){
-        let palabraFuera = palabras.pop();
-        palabras.push(palabra);
-        alert(palabras.join(" "));
-    }else{
-        salir =true;
-    }
-}while(!salir);
 
-alert(palabras.join (' '));
 /*
 Aleatorio 
 
@@ -72,7 +59,67 @@ let max, min;
 let numeroaleatorio = Math.floor(Math.random()* (max - min + 1)) + min;
 */
 
+/*
+EJERCICIO 1. 
+Escribe la función pedirNombres() que solicite al usuario una serie de nombres de propiedades CSS y se guarden en un array. 
+El tamaño del array se decidirá de manera dinámica, ya que se seguirán pidiendo nombres mientras que el usuario introduzca 
+cadenas de caracteres válidas (solo texto). Se parará de pedir nombres cuando el usuario introduzca un número. El array 
+creado se devuelve como resultado de la función.
+*/
 
+function pedirNombres() {
+
+    let palabras = [];
+    let condition = false;
+
+    do {
+        let palabra = prompt('palabra')
+        if(isNaN(palabra)){
+            palabras.push(palabra);
+        }else{
+            condition=true;
+        }
+    } while (!condition);
+    
+}
+
+/*
+EJERCICIO 2.
+Escribe la función camelize(str). Esta función recibe un único parámetro del tipo String. Se espera que la función convierta 
+palabras separadas por guion (por ejemplo, ‘border-left-radius’) en palabras con notación camelCase (por ejemplo, ‘borderLeftRadius’). 
+
+Es decir, que elimine todos los guiones y que cada palabra después de un guion comience con mayúscula (contando a partir de 
+la segunda palabra). El resultado de la conversión será devuelto por la función.
+*/
+
+function camelize(str){
+    let palabras = str.split('-');
+    
+    for (let i = 1; i < palabras.length; i++) {
+        let palabra = palabras[i];
+        let primeraLetra = palabra.charAt(0).toUpperCase();      
+        palabra = primeraLetra.concat(palabra.substring(1));
+        palabras[i]=palabra;
+    }
+    return palabras.join('');
+
+}
+
+let palabra = camelize(prompt('inserte su palabra aqu'))
+alert(palabra)
+
+/*
+EJERCICIO 3.
+Escribe la función sortear(arr) que recibe como parámetro un array de tamaño indeterminado y devuelve uno de los elementos 
+del array escogido al azar.
+*/
+
+/*
+EJERCICIO 4.
+Escribe una función principal() que llame al método realizado en el ejercicio 1, utilice la función del ejercicio 2 para 
+convertir todos los textos a la notación camelCase y termine eligiendo un elemento al azar utilizando la función del ejercicio 
+3. Este método debe ser llamado una vez ha finalizado la carga de la página web.
+*/
 
 
 
